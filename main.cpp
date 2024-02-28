558b6b4ac88d3b34371a74237c83233e68b66e19
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-02-27 19:56:13 -0800

    Move lose check to promptUser function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "User.h"
 
-void promptUser(bool, User*, User*);
+bool promptUser(bool, User*, User*);
 
 int main() {
     std::srand(time(NULL));
@@ -9,7 +9,7 @@ int main() {
     bool endGame = false;
     std::string winner;
 
-    Ship debugShip(5, 'o', true);
+    Ship debugShip(1, 'o', true);
     user.getBoard().setShip(0, 0, debugShip);
     user2.getBoard().setShip(0, 2, debugShip);
     std::cout << "User 1's board\n\n";
@@ -19,30 +19,21 @@ int main() {
 
     bool player1Turn = true;
     while (!endGame) {
-        promptUser(player1Turn, &user, &user2);
+        endGame = promptUser(player1Turn, &user, &user2);
         player1Turn = !player1Turn;
-
-        if (user2.isLoser()) {
-            endGame = true;
-            winner = "User 1 won!";
-        }
-
-        if (!endGame) {
-            promptUser(player1Turn, &user, &user2);
-            player1Turn = !player1Turn;
-
-            if (user.isLoser()) {
-                endGame = true;
-                winner = "User 2 won!";
-            }
-        }
     } 
+
+    if (player1Turn) {
+        winner = "User 2 won!";
+    } else {
+        winner = "User 1 won!";
+    }
     std::cout << std::endl << winner << std::endl;
 
     return 0;
 }
 
-void promptUser(bool player1Turn, User* player1, User* player2) {
+bool promptUser(bool player1Turn, User* player1, User* player2) {
     std::string player;
     User* currentPlayer;
     User* enemyPlayer;
@@ -65,4 +56,9 @@ void promptUser(bool player1Turn, User* player1, User* player2) {
     currentPlayer->getEmptyBoard().setCoordinate(x - 1, y - 1, hitChar);
     std::cout << "\nUser 2's board\n";
     currentPlayer->getEmptyBoard().print();
+
+    if (enemyPlayer->isLoser()) {
+        return true;
+    }
+    return false;
 }
